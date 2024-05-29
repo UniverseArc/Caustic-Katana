e6aa7c49d21a9cb9fe404ff80dbeff6c2d0d8008
universearc1 <orlov.playstation@yandex.ru>
2024-05-29 22:12:10 +0300

    first-touch-storybook

---
+++ src/components/typography/typography.stories.tsx
@@ -0,0 +1,22 @@
+import type { Meta, StoryObj } from '@storybook/react'
+import Typography from './typography'
+
+const meta: Meta<typeof Typography> = {
+  title: "UIKIT/Typography",
+  component: Typography,
+  argTypes: {
+    children: {
+      control: { type: "text" },
+    },
+  },
+  parameters: {
+    layout: "centered",
+  },
+};
+
+export default meta
+type Story = StoryObj<typeof Typography>
+
+export const Primary: Story = {
+  render: ({children = "Я текста!"}) => <Typography>{children}</Typography>,
+};
