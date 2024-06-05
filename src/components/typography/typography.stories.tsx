f2aaae3f4bc7c471593241fa0c6b816cbb3770e0
universearc1 <orlov.playstation@yandex.ru>
2024-06-05 22:26:51 +0300

    fix: fixed conflict

--- src/components/typography/typography.stories.tsx
+++ src/components/typography/typography.stories.tsx
@@ -18,9 +18,5 @@ export default meta
 type Story = StoryObj<typeof Typography>
 
 export const Primary: Story = {
-<<<<<<< Updated upstream
-  render: ({children = "Я шмекс"}) => <Typography>{children}</Typography>,
-=======
   render: ({children = "Я текста!", variant="hairline", tag="p"}) => <Typography variant={variant} tag={tag}>{children}</Typography>,
->>>>>>> Stashed changes
 };
